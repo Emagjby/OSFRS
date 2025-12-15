4eca4b67e1fef216cad18308e0601811d24b0601
Emagjby <emagjby@gmail.com>
2025-12-15 10:45:39 +0200

    feat: added landing page navbar and hero section

--- src/frontend/osfrs-frontend/src/lib/components/Hero.svelte
+++ src/frontend/osfrs-frontend/src/lib/components/Hero.svelte
@@ -7,27 +7,27 @@
 >
   <div>
     <h1
-      class="text-center text-[1.675rem] sm:text-4xl lg:text-5xl max-w-sm sm:max-w-xl lg:max-w-3xl font-semibold leading-tight"
+      class="text-center text-[1.675rem] xs:text-4xl lg:text-5xl max-w-sm xs:max-w-xl lg:max-w-3xl font-semibold leading-tight"
     >
       Online Sports Facility Reservation System
     </h1>
   </div>
   <div>
     <h2
-      class="text-center text-sm sm:text-lg lg:text-xl font-light max-w-3xs sm:max-w-xs lg:max-w-md text-text-secondary"
+      class="text-center text-sm xs:text-lg lg:text-xl font-light max-w-3xs xs:max-w-xs lg:max-w-md text-text-secondary"
     >
       Explore and reserve different types of sports facilities with ease.
     </h2>
   </div>
   <div
-    class="my-8 flex flex-col sm:flex-row justify-around items-center gap-4 w-full sm:w-auto"
+    class="my-8 flex flex-col xs:flex-row justify-around items-center gap-4 w-full xs:w-auto"
   >
     <a
-      class="flex justify-center items-center font-normal btn-neutral border-success-secondary bg-success text-md sm:text-lg px-6 py-3 w-full sm:w-auto"
+      class="flex justify-center items-center font-normal btn-neutral border-success-secondary bg-success text-md xs:text-lg px-6 py-3 w-full xs:w-auto"
       href={href("/facilities")}>Browse Facilities</a
     >
     <a
-      class="flex justify-center items-center font-medium btn-neutral text-md sm:text-lg px-6 py-3 w-full sm:w-auto"
+      class="flex justify-center items-center font-medium btn-neutral text-md xs:text-lg px-6 py-3 w-full xs:w-auto"
       href={href("/availability")}>Check Availability</a
     >
   </div>
