b0f04749f650af9d9a18012465d26b8bfa7539c2
Emagjby <emagjby@gmail.com>
2025-12-15 10:42:06 +0200

    wip

--- src/frontend/osfrs-frontend/src/lib/components/Hero.svelte
+++ src/frontend/osfrs-frontend/src/lib/components/Hero.svelte
@@ -2,7 +2,9 @@
   import { href } from "$lib/nav";
 </script>
 
-<div class="flex flex-col justify-center items-center gap-4 m-12 p-12 mx-auto">
+<div
+  class="flex flex-col justify-center items-center gap-4 mx-auto py-12 px-6 m-24 w-full"
+>
   <div>
     <h1
       class="text-center text-[1.675rem] sm:text-4xl lg:text-5xl max-w-sm sm:max-w-xl lg:max-w-3xl font-semibold leading-tight"
@@ -20,17 +22,13 @@
   <div
     class="my-8 flex flex-col sm:flex-row justify-around items-center gap-4 w-full sm:w-auto"
   >
-    <div>
-      <a
-        class="flex font-normal btn-neutral border-success-secondary bg-success text-lg px-6 py-3 w-full"
-        href={href("/facilities")}>Browse Facilities</a
-      >
-    </div>
-    <div>
-      <a
-        class="flex font-medium btn-neutral text-lg px-6 py-3 w-full"
-        href={href("/availability")}>Check Availability</a
-      >
-    </div>
+    <a
+      class="flex justify-center items-center font-normal btn-neutral border-success-secondary bg-success text-md sm:text-lg px-6 py-3 w-full sm:w-auto"
+      href={href("/facilities")}>Browse Facilities</a
+    >
+    <a
+      class="flex justify-center items-center font-medium btn-neutral text-md sm:text-lg px-6 py-3 w-full sm:w-auto"
+      href={href("/availability")}>Check Availability</a
+    >
   </div>
 </div>
--- src/frontend/osfrs-frontend/src/lib/components/LandingNavBar.svelte
+++ src/frontend/osfrs-frontend/src/lib/components/LandingNavBar.svelte
@@ -4,16 +4,18 @@
 
 <div class="flex flex-row justify-between p-4 max-w-6xl mx-auto" id="navbar">
   <div class="flex flex-row gap-4" id="left-navbar">
-    <p class="font-semibold text-xl">OSFRS</p>
+    <a class="logo-glass font-semibold text-xl cursor-pointer" href={href("/")}
+      >OSFRS</a
+    >
   </div>
 
   <div class="flex flex-row gap-4" id="right-navbar">
     <a
-      class="text-lg border-gray-700 border"
+      class="btn-neutral bg-register-fill"
       href={href("/auth", new URLSearchParams({ mode: "login" }))}>Login</a
     >
     <a
-      class="text-lg"
+      class="btn-neutral"
       href={href("/auth", new URLSearchParams({ mode: "register" }))}
       >Register</a
     >
