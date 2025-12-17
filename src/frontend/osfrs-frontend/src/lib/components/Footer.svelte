4a8452c5f8529c4f805a95e4821d9f9acb0ec2dd
Emagjby <emagjby@gmail.com>
2025-12-17 10:56:52 +0200

    wip: adding footer

---
+++ src/frontend/osfrs-frontend/src/lib/components/Footer.svelte
@@ -0,0 +1,23 @@
+<footer>
+  <div id="footer-top">
+    <div id="ft-left">
+      <h3>OSFRS</h3>
+      <h4>Reserve Sports Facilities</h4>
+    </div>
+    <div id="ft-right">
+      <a href="">Facilties</a>
+      <a href="">About</a>
+      <a href="">Contact</a>
+    </div>
+  </div>
+  <div id="footer-line"></div>
+  <div id="footer-bot">
+    <div id="fb-left">
+      <p>&copy; {new Date().getFullYear()} OSFRS. All rights reserved.</p>
+    </div>
+    <div id="fb-right">
+      <a href="">Privacy Policy</a>
+      <a href="">Terms of Service</a>
+    </div>
+  </div>
+</footer>
--- src/frontend/osfrs-frontend/src/routes/+layout.svelte
+++ src/frontend/osfrs-frontend/src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import "./layout.css";
   import favicon from "$lib/assets/favicon.svg";
+  import Footer from "$lib/components/Footer.svelte";
   import LandingNavBar from "$lib/components/LandingNavBar.svelte";
 
   let { children } = $props();
@@ -10,3 +11,4 @@
 
 <LandingNavBar />
 {@render children()}
+<Footer />
